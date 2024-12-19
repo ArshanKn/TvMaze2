de7d4f6752798cc235ef24369de6cbba5f01bb29
ArshanKn <arshan@mumit.org>
2024-12-19 10:52:06 -0800

    Update apiexample.js

--- apiexample.js
+++ apiexample.js
@@ -17,7 +17,7 @@ window.onload = function() {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('TvMaze2/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     }, function(error) {
       console.log('Service Worker registration failed:', error);
